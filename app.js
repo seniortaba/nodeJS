dec4a8a843243822d6ed58fcde42f696b0dda7d7
Taba <giotabatadze9@gmail.com>
2022-03-21 20:05:16 +0400

    დაემატა express.js როუტები for shopping project

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer((routes.handler));
-server.listen(3000);
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/', (req, res, next) => {
+    res.send('<p>main page</p>');
+})
+
+
+module.exports = router;
