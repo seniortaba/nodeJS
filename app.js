ac987c4e46708027c77e1ebad62d03cf737b5d48
Taba <giotabatadze9@gmail.com>
2022-03-23 19:24:04 +0400

    end express

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorRoute = require('./routes/404');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join('public')));
 
 app.use('/admin/', adminRoutes);
 
